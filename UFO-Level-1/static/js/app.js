aa3b74512267613ab96cb7b5d7759f28969c0552
Eric Viera <ericviera@Erics-MBP.home>
2020-10-21 22:06:48 -0400

    4th commit

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -8,3 +8,16 @@ var tbody = d3.select("tbody");
 
 // Console.log the data
 console.log(data);
+
+
+// Looping through and appending data rows on table
+data.forEach(function(ufo_data){
+    console.log(ufo_data);
+    var tableRow = tbody.append("tr");
+    
+    Object.entries(ufo_data).forEach(function([k,v]){
+        console.log(k,v)
+        var Cell = tableRow.append("td");
+        Cell.text(v);
+    })
+})
